427578081a3ba711b959721b97894330735414c9
Vitor Menezello <vitormenezello@gmail.com>
2017-05-14 19:47:51 -0300

    Update Player.java

--- player/Player.java
+++ player/Player.java
@@ -1,15 +1,18 @@
 package player;
 
 import deck.*;
+import printer.*;
 import java.util.ArrayList;
 
 public abstract class Player {
+    private Printer printer;
     private ArrayList<Card> hand;
     private int numCards;
 
     public Player() {
         this.hand = new ArrayList();
         this.numCards = 0;
+        this.printer = new Printer();
     }
 
     /* Get card at the top of the drawing stack */
@@ -27,6 +30,23 @@ public abstract class Player {
         return this.numCards;
     }
 
+    public Printer getPrinter(){
+        return this.printer;
+    }
+
+    public void playCard(int index){
+        this.hand.remove(index);
+        this.numCards--;
+    }
+
+    public Card getCard(int index){
+        return this.hand.get(index);
+    }
+
+    public abstract void invalidPlay();
+
+    public abstract String chooseSuit();
+
     /* Choose a card to play according to the top of the playing stack */
-    public abstract void playCard(Card openCard);
+    public abstract int chooseMove(Card topCard, boolean draw, boolean activeSeven, int totalSevens, String chosenSuit);
 }

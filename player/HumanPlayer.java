9238a0e2691c56656eadcafd4e64a7090d9025ab
Vitor Menezello <vitormenezello@gmail.com>
2017-05-15 19:16:31 -0300

    Update HumanPlayer.java
    Moved method validPlay to the class game

--- player/HumanPlayer.java
+++ player/HumanPlayer.java
@@ -7,42 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class HumanPlayer extends Player {
-    public boolean validCard(Card playedCard, Card topCard, boolean activeSeven, int totalSevens, String chosenSuit){
-        if ((topCard.getValue() == 7)&&(activeSeven)){
-            if (playedCard.getValue() == 7){
-                totalSevens++;
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else if (topCard.getValue() == 11){
-            if (playedCard.getSuit() == chosenSuit){
-                return true;
-            }
-            else if (playedCard.getValue() == 11){
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else if (topCard.getValue() == playedCard.getValue()){
-            return true;
-        }
-        else if (topCard.getSuit() == playedCard.getSuit()){
-            return true;
-        }
-        else if (playedCard.getValue() == 11){
-            return true;
-        }
-        else {
-            this.getPrinter().printInvalid();
-            return false;
-        }
-    }
-
+    // Input methods
     public int readCardInput (int size){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = null;
@@ -74,9 +39,15 @@ public class HumanPlayer extends Player {
         return input;
     }
 
+    // Choice and printing methods
     @Override
     public void invalidPlay(){
         this.getPrinter().printInvalid();
+    }
+
+    @Override
+    public void validPlay() throws InterruptedException {
+        Thread.sleep(0);
     }
 
     @Override
@@ -88,20 +59,13 @@ public class HumanPlayer extends Player {
     @Override
     public int chooseMove(Card topCard, boolean draw, boolean activeSeven, int totalSevens, String chosenSuit) {
         int index;
-        do {
-            // Print options
-            this.getPrinter().printTopCard(topCard);
-            this.getPrinter().printOptions(this.getHand(), draw);
 
-            // Read input from player
-            index = readCardInput (getHand().size());
-            // If player wants to draw/skip
-            if (index == 0){
-                break;
-            }
+        // Print options
+        this.getPrinter().printTopCard(topCard);
+        this.getPrinter().printOptions(this.getHand(), draw);
 
-        } while (!validCard(this.getCard(index - 1), topCard, activeSeven, totalSevens, chosenSuit));
-
+        // Read input from player
+        index = readCardInput (getHand().size());
 
         return index - 1;
     }

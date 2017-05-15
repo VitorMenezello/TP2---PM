45d43d12eaacdf3a683259f713bf1cc92048265c
Vitor Menezello <vitormenezello@gmail.com>
2017-05-15 19:19:13 -0300

    Update Player.java

--- player/Player.java
+++ player/Player.java
@@ -15,7 +15,8 @@ public abstract class Player {
         this.printer = new Printer();
     }
 
-    /* Get card at the top of the drawing stack */
+    /* Get methods */
+    // Get the card at the top of the drawing stack
     public void drawCard(CardDeck drawingStack) {
         Card card = drawingStack.drawCard();
         this.hand.add(card);
@@ -34,19 +35,24 @@ public abstract class Player {
         return this.printer;
     }
 
+    public Card getCard(int index){
+        return this.hand.get(index);
+    }
+
+    // Removes the played card from the hand
     public void playCard(int index){
         this.hand.remove(index);
         this.numCards--;
     }
 
-    public Card getCard(int index){
-        return this.hand.get(index);
-    }
-
+    /* Reaction to valid or invalid plays methods */
     public abstract void invalidPlay();
 
+    public abstract void validPlay() throws InterruptedException;
+
+    /* Choice methods */
     public abstract String chooseSuit();
 
-    /* Choose a card to play according to the top of the playing stack */
+    // Choose a card to play according to the top of the playing stack
     public abstract int chooseMove(Card topCard, boolean draw, boolean activeSeven, int totalSevens, String chosenSuit);
 }
